26fb92a4aa6b8464146558b5e9c278dfc8b53be5
rasitesdmr <mrasitesdemir7@gmail.com>
2022-07-23 18:00:47 +0300

    CreationalDesignPatterns

--- SingletonPattern/SingletonPattern/src/baseSingleton/BaseSingleton.java
+++ SingletonPattern/SingletonPattern/src/baseSingleton/BaseSingleton.java
@@ -1,4 +1,20 @@
 package baseSingleton;
 
 public class BaseSingleton {
+  private static BaseSingleton baseSingleton = new BaseSingleton();
+
+  private static int count;
+  private String name;
+
+    public BaseSingleton() {
+        count++;
+        name = "Singleton" + count;
+    }
+
+    public static BaseSingleton getBaseSingleton() {
+        return baseSingleton;
+    }
+    public void printName(){
+        System.out.println(name);
+    }
 }
--- SingletonPattern/SingletonPattern/src/baseSingleton/Main.java
+++ SingletonPattern/SingletonPattern/src/baseSingleton/Main.java
@@ -1,4 +1,21 @@
 package baseSingleton;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <100 ; i++) {
+            BaseSingleton baseSingleton  = BaseSingleton.getBaseSingleton();
+            baseSingleton.printName();
+        }
+        BaseSingleton s1 = BaseSingleton.getBaseSingleton();
+        System.out.println("\nHashcode: " + s1.hashCode());
+
+        BaseSingleton s2 = BaseSingleton.getBaseSingleton();
+        System.out.println("\nHashcode: " + s2.hashCode());
+
+        if (s1 == s2){
+            System.out.println("Aynı tür nesneler");
+        }else {
+            System.out.println("Farklı tür nesneler");
+        }
+    }
 }
